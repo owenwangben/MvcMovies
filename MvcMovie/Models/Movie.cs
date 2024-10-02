9e91f170b069c11a1418b062b62cdf90be27fc44
owen.wang <try09067888@gmail.com>
2024-10-02 18:25:06 +0800

    first

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType("DataType.Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [StringLength(5)]
+        public string Rating { get; set; }
+    }
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
